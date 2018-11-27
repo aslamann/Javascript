9d1fc372c0ea7f8ad72d1af3d803c75084220543
aslamann <aslaman@roc-dev.com>
2018-11-27 11:37:52 +0100

    dit is mijn javascript

---
+++ 005/llamachooser.js
@@ -0,0 +1,18 @@
+pics = document.getElementById("pics");
+createPicsHolders();
+
+function createPicsHolders() {
+    for(var 1 = 0; 1 < 9; i++) {
+        pictureHolder = document.createElement("div");
+        pictureHolder.className = "llamapicture";
+        pictureHolder.id = "picture-holder" + i;
+        pics.appendChild(pictureHolder);
+    }
+}
+
+
+//picsHolders maken
+//picsHolder bevat een plaatje van een lama en een favorite symbool (hartje)
+
+//lamaPlaatjes toevoegen aan picsHolders
+//favoriteSymbools
