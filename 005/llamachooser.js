1b885c209e178d6dc0d6e19bd31c84c3d3118f9d
aslamann <aslaman@roc-dev.com>
2018-12-03 10:01:03 +0100

    dit is mijn javascript

--- 005/llamachooser.js
+++ 005/llamachooser.js
@@ -1,15 +1,26 @@
 pics = document.getElementById("pics");
 createPicsHolders();
+createLlamaImages ();
 
 function createPicsHolders() {
     for(var i = 0; i < 9; i++) {
         pictureHolder = document.createElement("div");
-        pictureHolder.className = "llamapicture";
+        pictureHolder.className = "picture-holder";
         pictureHolder.id = "picture-holder-" + i;
         pics.appendChild(pictureHolder);
     }
 }
 
+function createLlamaImages() {
+    pictureHolders = document.getElementsByClassName("picture-holder");
+  for(var i = 0; i < pictureHolders.length; i++) {
+      favoriet = document.createElement("div");
+
+      lamaPlaatje = document.createElement("img");
+      lamaPlaatje.src = "pictures005/lama" + (i+1) + ".jpg";
+      pictureHolders[i].appendChild(lamaPlaatje);
+  }
+}
 
 //picsHolders maken
 //picsHolder bevat een plaatje van een lama en een favorite symbool (hartje)
